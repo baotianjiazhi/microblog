60fe67d01b1e332eae7b9aa975f7a8662930fe75
bao <524502256@qq.com>
2018-09-13 14:11:41 +0800

    样式修改

--- microblog.py
+++ microblog.py
@@ -16,3 +16,7 @@ cli.register(app)
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Messages': Message, 'Notification': Notification}
+
+
+if __name__ == '__main__':
+    app.run()
